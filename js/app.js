1fca0add15d5a2df4f3d826557356bd0225e04d3
DarrenLute <InfinitePoss@gmail.com>
2014-05-14 14:03:16 -0700

    Update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,53 @@
-/**
- *
- * Responsive website using AngularJS
- * http://www.script-tutorials.com/responsive-website-using-angularjs/
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Script Tutorials
- * http://www.script-tutorials.com/
- */
-
 'use strict';
+
+$(function() {
+    var menu = $('#navigation-menu');
+    var menuToggle = $('#js-mobile-menu');
+
+    $(menuToggle).on('click', function (e) {
+        e.preventDefault();
+        menu.slideToggle(function () {
+            if (menu.is(':hidden')) {
+                menu.removeAttr('style');
+            }
+        });
+    });
+});
+
+$(".js-vertical-tab-content").hide();
+$(".js-vertical-tab-content:first").show();
+
+/* if in tab mode */
+$(".js-vertical-tab").click(function (event) {
+    event.preventDefault();
+
+    $(".js-vertical-tab-content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).show();
+
+    $(".js-vertical-tab").removeClass("is-active");
+    $(this).addClass("is-active");
+
+    $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+    $(".js-vertical-tab-accordion-heading[rel^='" + activeTab + "']").addClass("is-active");
+});
+
+/* if in accordion mode */
+$(".js-vertical-tab-accordion-heading").click(function (event) {
+    event.preventDefault()
+
+    $(".js-vertical-tab-content").hide();
+    var accordion_activeTab = $(this).attr("rel");
+    $("#" + accordion_activeTab).show();
+
+    $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+    $(this).addClass("is-active");
+
+    $(".js-vertical-tab").removeClass("is-active");
+    $(".js-vertical-tab[rel^='" + accordion_activeTab + "']").addClass("is-active");
+});
+
+
 
 var app = angular.module("dlApp", ["ngRoute"]);
 
